dc686320a03faffa2076e2b5469c26f92e0c4b94
Patrick Charles <patrickrcharles@gmail.com>
2024-07-14 22:12:44 -0500

    api returning data to frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Paper, { paperClasses } from '@mui/material/Paper';
 import { CardMedia, Grid } from '@mui/material';
+import HighScoreApi from './api/HighScoreApi';
 
 export default function App() {
 
   // call api
   fetchHighscores();
-  console.log("hello");
   return (
     <Box sx={{ flexGrow: 1 }} >
       <Grid container sx={{ padding: "1em 1em 0 1em" }}
@@ -31,23 +27,7 @@ export default function App() {
     </Box>
   );
 }
-async function fetchHighscores() {
-
-//   const headers = {'Content-Type':'application/json',
-//     'Access-Control-Allow-Origin':'*',
-//     'Access-Control-Allow-Methods':'POST,PATCH,OPTIONS'}
-// const response = {
-// statusCode: 200,
-// headers:headers,
-// body: JSON.stringify(X),
-// };
-// return response;
-
-  console.log("fetchHighscores");
-    const response = await fetch('https://api.sweatthis.com/api/weather');
-    console.log("response: ", response);
-    const data = await response.json();
-    console.log("data: ", data);
-    return data;
+async function fetchHighscores(this: any) {
+  return HighScoreApi.get();
 }
 
--- src/api/HighScoreApi.ts
+++ src/api/HighScoreApi.ts
@@ -2,16 +2,39 @@
 import * as React from 'react';
 
 
-class HighScoreApi{
+class HighScoreApi {
 
 
-  async fetchHighscores(): Promise<Highscore[]> {
+  async fetchHighscores(url: string): Promise<Highscore[]> {
     console.log("fetchHighscores");
-    const response = await fetch('https://localhost:44304/api/highscores');
+    const response = await fetch(url);
     const data = await response.json();
     console.log("data: ", data);
     return data;
   }
 
+  async get(requestOptions?: { headers: Headers; }): Promise<Highscore[] | String | any> {
+
+    const request = new Request('http://api.sweatthis.com/api/highscores');
+
+    // const response = await fetch('http://api.sweatthis.com/api/highscores',
+    //   {
+    //     mode: 'no-cors',
+    //     method: "get",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       "Access-Control-Allow-Origin": "*",
+    //       "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+
+    //     },
+    //     credentials:"include"
+    const response = await fetch(request);
+
+    var data = await response.json();
+    console.log(data);
+    console.log(response);
+    return data;
+  }
 }
-export default new HighScoreApi()
+
+export default new HighScoreApi();
