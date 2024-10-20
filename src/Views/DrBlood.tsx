fe138283454b373d3b4b9f2bbb4019d1fc08c096
Patrick Charles <patrickrcharles@gmail.com>
2024-10-20 16:05:45 -0500

    add youtube vid links

--- src/Views/DrBlood.tsx
+++ src/Views/DrBlood.tsx
@@ -3,13 +3,14 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { useState } from 'react';
 import HighScoreApi from '../api/HighScoreApi';
 import ReactPlayer from 'react-player';
-import dbPlay from '/video/db_playGame.mp4'
-import dbFootage from '/video/db_footage.mp4'
-  
+import dbPlay from '/video/db_playGame.mp4';
+import dbFootage from '/video/db_footage.mp4';
+import YouTube from 'react-youtube';
+
 export default function DrBlood() {
 
-  const loaderData = fetchHighscores(); 
-//   const loaderData =  HighScoreApi.get();
+  const loaderData = fetchHighscores();
+  //   const loaderData =  HighScoreApi.get();
   console.log("--------", loaderData);
   const navigate = useNavigate();
   const params = useParams();
@@ -17,13 +18,22 @@ export default function DrBlood() {
 
   return (
         <Grid>
-          <ReactPlayer controls autostart autoPlay url={dbPlay} />
-          <ReactPlayer controls autostart autoPlay url={dbFootage} />
+          {/* <ReactPlayer controls autostart autoPlay url={dbPlay} />
+          <ReactPlayer controls autostart autoPlay url={dbFootage} /> */}
+          <YouTube videoId="TY44PEt4378"/>
+          <YouTube videoId="Fx3nlW8gf_Y"/>
+          <YouTube videoId="TjtovD-Sb5E"/>
+          <YouTube videoId="3Y-kf_PkwZg"/>
+          <YouTube videoId="8R8f5mcSj1k"/>
+          <YouTube videoId="uaMFY0477uQ"/>
+          <YouTube videoId="e2Bx2lv33Yo"/>
+          <YouTube videoId="ScO5wpZAvvY"/>
         </Grid>
+        
   );
 }
 
 async function fetchHighscores(this: any) {
-    return HighScoreApi.get();
-  }
+  return HighScoreApi.get();
+}
 
